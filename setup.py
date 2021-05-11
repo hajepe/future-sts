caf516c5b4d4e754801ba539bb2d4117ac5fefc8
Altanbagana <saruul.altanbagana.ext@siemens-healthineers.com>
2021-05-11 10:54:21 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predict Future Sales with Time Series',
+    author='Saruul Altanbagana',
+    license='MIT',
+)
